91dbbd3596ff96adac128b3c01923ce416d95a90
Massudesu <nicolasmassuda01@gmail.com>
2026-04-29 14:52:19 -0300

    Fazendo exercicio 7

--- projetos_java/aula13/Aula13/src/appCaixaEletronico_exerc7.java
+++ projetos_java/aula13/Aula13/src/appCaixaEletronico_exerc7.java
@@ -9,5 +9,31 @@
 //Repetir até sair.
 import static java.lang.IO.*;
 void main() {
-    
+    float saldo = 500;
+    float resu=0;
+    while(true){
+        println("Qual serviço quer acessar hoje?");
+        println("1 Saque");
+        println("2 Deposito");
+        println("3 Saldo");
+        println("0 Sair");
+        String opcao =IO.readln();
+
+        switch (opcao) {
+            case "0":
+                println("Saindo....");
+                break;
+        }
+        switch (opcao){
+            case "1":
+                float sacar=Float.parseFloat(IO.readln("Você tem R$"+ String.format("%.2f",saldo)+" Quanto quer sacar: "));
+                resu= saldo - sacar;
+                println("Você sacou R$"+sacar);
+                println("Seu novo saldo é R$"+resu);
+        }
+        switch (opcao){
+            case "3":
+                println("Seu saldo é R$"+String.format("%.2f",saldo));
+        }
+    }
 }

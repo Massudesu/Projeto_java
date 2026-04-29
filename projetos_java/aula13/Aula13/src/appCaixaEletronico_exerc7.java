95c1b2bfb4a081a026bfc3319feb96febe5d6835
Massudesu <nicolasmassuda01@gmail.com>
2026-04-29 16:38:48 -0300

    arrumado_v2

--- projetos_java/aula13/Aula13/src/appCaixaEletronico_exerc7.java
+++ projetos_java/aula13/Aula13/src/appCaixaEletronico_exerc7.java
@@ -19,34 +19,44 @@ void main() {
         println("0 Sair");
         String opcao =IO.readln();
 
-        switch (opcao) {
-            case "0":
-                println("Saindo....");
-                break;
-        }
+
         switch (opcao){
             case "1":
                 float sacar=Float.parseFloat(IO.readln("Você tem R$"+ String.format("%.2f",saldo)+" Quanto quer sacar: "));
-                resu= saldo - sacar;
-                println("Você sacou -R$"+String.format("%.2f",sacar));
-                saldo -= sacar;
-                println("Seu novo saldo é R$"+String.format("%.2f",resu));
-                println("--------------------------------------");
+                if(sacar > saldo){
+                    println("Saque maior que o saldo, cancelando....");
+                }else{
+                    resu= saldo - sacar;
+                    println("Você sacou -R$"+String.format("%.2f",sacar));
+                    saldo -= sacar;
+                    println("Seu novo saldo é R$"+String.format("%.2f",resu));
+                    println("--------------------------------------");
+                }
+
 
         }
         switch (opcao){
             case "2":
-                float depos=Float.parseFloat(IO.readln("Você tem R$"+ String.format("%.2f",saldo)+" Quanto quer depositar: "));
-                resu= saldo + depos;
-                println("Você depositou +R$"+String.format("%.2f",depos));
-                saldo += depos;
-                println("Seu novo saldo é R$"+String.format("%.2f",resu));
-                println("--------------------------------------");
+                float deposito=Float.parseFloat(IO.readln("Você tem R$"+ String.format("%.2f",saldo)+" Quanto quer depositar: "));
+                if(deposito>10000){
+                    println("Você so pode depositar ate R$10.000, cancelando....");
+                }else{
+                    resu= saldo + deposito;
+                    println("Você depositou +R$"+String.format("%.2f",deposito));
+                    saldo += deposito;
+                    println("Seu novo saldo é R$"+String.format("%.2f",resu));
+                    println("--------------------------------------");
+                }
+
         }
         switch (opcao){
             case "3":
                 println("Seu saldo é R$"+String.format("%.2f",saldo));
                 println("--------------------------------------");
+        }
+        if(opcao.equals("0")){
+            println("Saindo....");
+            break;
         }
     }
 }

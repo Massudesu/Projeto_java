84e89c7eca3400cf48bd818766ac40bc82c3f5b0
Massudesu <nicolasmassuda01@gmail.com>
2026-05-24 22:04:39 -0300

    exercicio 15

--- projetos_java/aula15/Aula15/src/exerc5_lista_compras.java
+++ projetos_java/aula15/Aula15/src/exerc5_lista_compras.java
@@ -11,19 +11,96 @@
 //     exibir a mensagem "Lista vazia" senão exibir os produtos cadastrados.
 //     para a opção 6 sair do programa e exibir na tela "Programa encerrado com sucesso!".
 import static java.lang.IO.*;
+ArrayList<String> listaCompras = new ArrayList<>();
+
 void main() {
-    while(true){
+    while (true) {
+        println("LISTA DE COMPRAS");
         println("1 - Adicionar a lista");
         println("2 - Pesquisar item");
-        println("3 - remover item");
+        println("3 - Remover item");
         println("4 - Alterar item");
         println("5 - Listar produtos");
         println("6 - Sair");
+        print("Escolha uma opção: ");
         String opcao = readln();
 
-        if(opcao.equals("6")){
-            println("Programa encerrado com sucesso!");
-            break;
+        switch (opcao) {
+            case "1":
+                println("Adicionar Itens (digite 'sair' para voltar)");
+                while (true) {
+                    print("Digite o produto: ");
+                    String produto = readln().trim();
+                     if (produto.equalsIgnoreCase("sair")) {
+                         break;
+                     }
+
+                     if (!produto.isEmpty()) {
+                         listaCompras.add(produto);
+                         println(produto + " adicionado!");
+                     }
+                }
+                break;
+
+                case "2":
+                    println("Pesquisar Item");
+                    print("Digite o produto para pesquisar: ");
+                    String pesquisar = readln().trim();
+
+                    if (listaCompras.contains(pesquisar)) {
+                        println("Produto encontrado: " + pesquisar);
+                    } else {
+                        println("Produto não encontrado");
+                    }
+                    break;
+
+                    case "3":
+                        println("Remover Item ");
+                        print("Digite o produto a ser removido: ");
+                        String remover = readln().trim();
+
+
+                        if (listaCompras.remove(remover)) {
+                            println("Produto encontrado e removido com sucesso!");
+                        } else {
+                            println("Produto não encontrado");
+                        }
+                        break;
+
+                        case "4":
+                            println("Alterar Item");
+                            print("Digite o nome do produto que deseja alterar: ");
+                            String antigo = readln().trim();
+
+                            int indice = listaCompras.indexOf(antigo);
+                            if (indice != -1) {
+                                print("Digite o novo nome para o produto: ");
+                                String novo = readln().trim();
+                                if (!novo.isEmpty()) {
+                                    listaCompras.set(indice, novo);
+                                    println("Produto alterado com sucesso!");
+                                }
+                            } else {
+                                println("Produto não encontrado");
+                            }
+                            break;
+
+                            case "5":
+                                println("Produtos Cadastrados");
+                                if (listaCompras.isEmpty()) {
+                                    println("Lista vazia");
+                                } else {
+                                    for (int i = 0; i < listaCompras.size(); i++) {
+                                        println((i + 1) + ". " + listaCompras.get(i));
+                                    }
+                                }
+                                break;
+                                case "6":
+                                    println("Programa encerrado com sucesso!");
+                                    return;
+                                    default:
+                                        println("Opção inválida! Tente novamente.");
+                                        break;
         }
     }
 }
